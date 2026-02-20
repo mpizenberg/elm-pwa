90b1e1e320f0eac3f6e2c73e9e4e759178092fec
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2026-02-20 21:15:50 +0100

    Bust cache
    Signed-off-by: Matthieu Pizenberg <matthieu.pizenberg@gmail.com>

--- examples/demo/build-sw.mjs
+++ examples/demo/build-sw.mjs
@@ -4,7 +4,7 @@ import { writeFileSync } from "node:fs";
 writeFileSync(
   "static/sw.js",
   generateSW({
-    cacheName: "elm-pwa-v5",
+    cacheName: "elm-pwa-v6",
     precacheUrls: [
       "/",
       "/elm.js",
